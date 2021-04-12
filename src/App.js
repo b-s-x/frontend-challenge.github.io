cad3e62107361554474fb48a24e6cfa8dad67c0f
b-s-x <b.s.x.mars@gmail.com>
2021-04-12 12:32:26 +0300

    style fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 
-import {AllCatsPage} from './page/AllCatsPage'
-import {CatHeader} from './components/CatHeader'
-import {FavoriteCatsPage} from './page/FavoriteCatsPage'
+import { AllCatsPage } from './page/AllCatsPage'
+import { CatHeader } from './components/CatHeader'
+import { FavoriteCatsPage } from './page/FavoriteCatsPage'
 
 
 function App() {
   return (
     <div>
-        <BrowserRouter basename="/frontend-challenge.github.io">
-          <CatHeader />
+      <BrowserRouter basename="/frontend-challenge.github.io">
+        <CatHeader />
 
-          <Switch>
-            <Route exact path="/" component={AllCatsPage} />
-            <Route exact path="/favorites" component={FavoriteCatsPage} />
-          </Switch>
-        </BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={AllCatsPage} />
+          <Route exact path="/favorites" component={FavoriteCatsPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
